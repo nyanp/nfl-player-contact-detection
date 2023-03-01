71201b84bbdf5cf4887d6b7b6ba7de340bce097d
bamps53 <daisuke0530@gmail.com>
2023-03-01 04:18:26 +0000

    exp095

--- main.py
+++ main.py
@@ -324,7 +324,7 @@ def inference(cfg: Config):
 
 def main(args):
     cfg = Config(
-        EXP_NAME='exp094_camaro_exp188',
+        EXP_NAME='exp095_camaro_exp188_last',
         PRETRAINED_MODEL_PATH=args.lgbm_path,
         CAMARO_DF1_PATH=args.camaro1_path,
         CAMARO_DF1_ANY_PATH=args.camaro1_any_path,
